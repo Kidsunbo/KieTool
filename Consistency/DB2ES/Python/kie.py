f68b7fee6b3de430feefd7cc48bde1aea444264a
sunbo.kie <sunbo.kie@bytedance.com>
2020-06-23 15:53:51 +0800

    push time as timestamp

--- Consistency/DB2ES/Python/kie.py
+++ Consistency/DB2ES/Python/kie.py
@@ -32,7 +32,7 @@ def read_from_database(db_setting):
     with con.cursor() as cur:
         cur.execute("SELECT * FROM t_order_base")
         for row in cur.fetchall():
-            data.append({cur.description[i][0]: str(row[i]) if type(row[i]) in [datetime.datetime] else row[i] for i in range(len(row))})
+            data.append({cur.description[i][0]: int(datetime.datetime.timestamp(row[i])) if type(row[i]) in [datetime.datetime] else row[i] for i in range(len(row))})
     con.close()
     return data
 
