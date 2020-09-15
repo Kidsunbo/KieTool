e7a6ab103fd24e35c3b94ebdd267421de371425f
sunbo.kie <sunbo.kie@bytedance.com>
2020-09-15 11:17:30 +0800

    change the url

--- KieShop/ToolReception/main.cpp
+++ KieShop/ToolReception/main.cpp
@@ -10,7 +10,7 @@ int main() {
     std::size_t max_thread_num = std::thread::hardware_concurrency();
     http_server server(max_thread_num);
     server.listen("0.0.0.0", "12138");
-    server.set_http_handler<GET, POST>("/", handler::shardingKeyHandler);
+    server.set_http_handler<GET, POST>("/api/tool/sharding-key", handler::shardingKeyHandler);
     SPDLOG_INFO("start server");
     server.run();
     return 0;
